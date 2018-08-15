221cf4e94dcfcd56e33eba276bda38d674e95f4d
super <zxpscau@163.com>
2018-08-15 19:20:15 +0800

    update

---
+++ quickSort.js
@@ -0,0 +1,4 @@
+// 原地交换函数，而非用临时数组
+function swap(array, a, b) {
+  [array[a], array[b]] = [array[b], array[a]];
+}
--- ruanyifengSort.js
+++ ruanyifengSort.js
@@ -14,7 +14,7 @@ function generateArr(len) {
 
 // 统计占用了多少空间
 let sum = 0;
-
+let  t= 0;
 var quickSort = function(arr) {
   if (arr.length <= 1) {
     return arr;
@@ -29,6 +29,7 @@ var quickSort = function(arr) {
   var right = [];
 
   for (var i = 0; i < arr.length; i++) {
+    t++
     if (arr[i] < pivot) {
       left.push(arr[i]);
     } else {
@@ -40,10 +41,12 @@ var quickSort = function(arr) {
 };
 // 生成十万个成员的数组
 generateArr(100000);
-
-// 将数组反向排序,目的是使得接下来的快排达到最差情况,也就是O(n㏒n)的复杂度
 arr.sort((a, b) => b - a);
 
+// 将数组反向排序,目的是使得接下来的快排达到最差情况,也就是O(n㏒n)的复杂度
+
+console.time('super')
 quickSort(arr);
+console.timeEnd('super')
 
 console.log(sum); // 1481308 近150万
