9b0fa0719a935168f9fb498627b1800244bb1d4a
super <zxpscau@163.com>
2018-08-17 19:33:19 +0800

    5

--- test.js
+++ test.js
@@ -1,9 +1,10 @@
-var arr1 = "john".split('');
-console.log(arr1)
-var arr2 = arr1.reverse();
-var arr3 = "jones".split('');
-arr2.push(arr3);
-console.log(arr1)
-console.log(arr2)
-console.log("array 1: length=" + arr1.length + " last=" + arr1.slice(-1));
-console.log("array 2: length=" + arr2.length + " last=" + arr2.slice(-1));
+var d = 'Tom';
+function zeta() {
+    
+    if (d === 'ss') {
+        let d = 'Jerry';
+        console.log(d);
+    }
+    console.log(d);
+}
+zeta();
