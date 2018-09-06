3e04fd846ff1cb80c0e27115b65444acd09a3121
super <zxpscau@163.com>
2018-09-06 19:19:25 +0800

    0

---
+++ linkTable.js
@@ -0,0 +1,56 @@
+class Node {
+  constructor(element) {
+    this.element = element; //当前节点的元素
+    this.next = null; //下一个节点链接
+  }
+}
+
+class linkTable {
+  constructor() {
+    this.head = new Node("head");
+  }
+  // 寻找节点
+  find(item) {
+    var currNode = this.head;
+    while (currNode.element != item) {
+      currNode = currNode.next;
+    }
+    return currNode;
+  }
+  // 在 item 之后插入 newElement 节点
+  insert(newElement, item) {
+    var newNode = new Node(newElement);
+    var currNode = this.find(item);
+    newNode.next = currNode.next;
+    currNode.next = newNode;
+  }
+  // 显示链表
+  display() {
+    var currNode = this.head;
+    while (currNode.next !== null) {
+      console.log(currNode.next.element);
+      console.log(t);
+      currNode = currNode.next;
+    }
+  }
+  // 找前一个
+  findPrev(item) {
+    var currNode = this.head;
+    while (!(currNode.next == null) && currNode.next.element != item) {
+      currNode = currNode.next;
+    }
+    return currNode;
+  }
+  // 移除
+  remove(item) {
+    var prevNode = this.findPrev(item);
+    if (!(prevNode.next == null)) {
+      prevNode.next = prevNode.next.next;
+    }
+  }
+}
+var fruits = new linkTable();
+
+fruits.insert("Apple", "head");
+fruits.insert("Banana", "Apple");
+fruits.insert("Pear", "Banana");
