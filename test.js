410ab61fe1a5361efd39a227082043f7a3fa7778
郑晓鹏 <zxpscau@163.com>
2020-06-17 17:43:51 +0800

    update

--- test.js
+++ test.js
@@ -1,47 +1,40 @@
-!(function(e) {
-    function t(a) {
-        if (i[a]) return i[a].exports;
-        var n = (i[a] = { exports: {}, id: a, loaded: !1 });
-        return (
-            e[a].call(n.exports, n, n.exports, t), (n.loaded = !0), n.exports
-        );
-    }
-    var i = {};
-    return (t.m = e), (t.c = i), (t.p = ''), t(0);
-})([
-    function(e, t) {
-        'use strict';
-        Object.defineProperty(t, '__esModule', { value: !0 });
-        var i = window;
-        (t['default'] = i.flex = function(e, t) {
-            var a = e || 100,
-                n = t || 1,
-                r = i.document,
-                o = navigator.userAgent,
-                d = o.match(/Android[\S\s]+AppleWebkit\/(\d{3})/i),
-                l = o.match(/U3\/((\d+|\.){5,})/i),
-                c = l && parseInt(l[1].split('.').join(''), 10) >= 80,
-                p = navigator.appVersion.match(/(iphone|ipad|ipod)/gi),
-                s = i.devicePixelRatio || 1;
-            p || (d && d[1] > 534) || c || (s = 1);
-            var u = 1 / s,
-                m = r.querySelector('meta[name="viewport"]');
-            m ||
-                ((m = r.createElement('meta')),
-                m.setAttribute('name', 'viewport'),
-                r.head.appendChild(m)),
-                m.setAttribute(
-                    'content',
-                    'width=device-width,user-scalable=no,initial-scale=' +
-                        u +
-                        ',maximum-scale=' +
-                        u +
-                        ',minimum-scale=' +
-                        u
-                ),
-                (r.documentElement.style.fontSize = (a / 2) * s * n + 'px');
-        }),
-            (e.exports = t['default']);
-    },
-]);
-flex(100, 1);
+class Database { 
+    select(sql) { 
+        const mysql = require('mysql'); 
+        return new Promise(resolve => { 
+            // 连接数据库，并执行 sql 语句进行查询 
+            mysql.createConnection().query(sql, (error, results, fields) => { 
+                const success = results.length > 0 ? true : false; 
+                resolve(success); 
+            }); 
+        }); 
+    } 
+}
+
+class Service { 
+    constructor(db) { this.db = db; } 
+    async login(username, password) { 
+        // 
+        const db = new Database(); 
+        const success = await this.db.select(select * from user where username=${username} and password=${password} ); 
+        return success ? '登录成功' : '登录失败'; 
+    } 
+}
+
+class Web { 
+    constructor(service) { this.service = service; } 
+    matchRouter(path) { 
+        switch (path) { 
+            case 'login': // 
+            const service = new Service(); 
+            const { username, password } = path.query; 
+            return this.service.login(username, password); 
+        } 
+    } 
+}
+
+// 使用 web 层之前，必须手动创建依赖，并注入 
+const database = new Database(); 
+const service = new Service(database); 
+const web = new Web(service); 
+web.matchRouter('login');
